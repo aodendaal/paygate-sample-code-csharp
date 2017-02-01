eb6906b67d3494d5df64d1bc7dae6be18268d026
Andre Odendaal <aodendaal@gmail.com>
2017-02-01 12:03:36 +0200

    Redirect user to payment portal

--- PaygateTest/Controllers/PaygateController.cs
+++ PaygateTest/Controllers/PaygateController.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Collections.Generic;
     using System.Linq;
-    using System.Net;
     using System.Net.Http;
     using System.Security.Cryptography;
     using System.Text;
@@ -13,6 +12,11 @@
 
     public class PaygateController : ApiController
     {
+        /// <summary>
+        /// Initiates the Paygate Payweb request
+        /// </summary>
+        /// <returns>Dictionary of values from Paygate</returns>
+        [HttpGet]
         public async Task<IHttpActionResult> GetRequest()
         {
             var client = new HttpClient();
@@ -20,14 +24,14 @@
             var request = new Dictionary<string, string>();
 
             request.Add("PAYGATE_ID", "10011072130");
-            request.Add("REFERENCE", "PayGate Test");
-            request.Add("AMOUNT", "3299");
+            request.Add("REFERENCE", "AppFactory Ransom");
+            request.Add("AMOUNT", "100000000");
             request.Add("CURRENCY", "ZAR");
-            request.Add("RETURN_URL", "https://www.paygate.co.za/thankyou");
-            request.Add("TRANSACTION_DATE", "2016-03-10 10:49:16");
+            request.Add("RETURN_URL", "http://localhost:5403/api/paygate");
+            request.Add("TRANSACTION_DATE", "2017-02-01 11:45:16");
             request.Add("LOCALE", "en-za");
             request.Add("COUNTRY", "ZAF");
-            request.Add("EMAIL", "customer@paygate.co.za");
+            request.Add("EMAIL", "v-anoden@microsoft.com");
 
             request.Add("CHECKSUM", GetMd5Hash(request, "secret"));
 
@@ -46,13 +50,21 @@
                 return InternalServerError(new Exception(results["ERROR"]));
             }
 
+            if (!VerifyMd5Hash(results, "secret", results["CHECKSUM"]))
+            {
+                return InternalServerError(new Exception("MD5 verification failed"));
+            }            
+
             return Ok(results);
         }
 
-        public IHttpActionResult PostResult([FromBody]string body)
+        [HttpPost]
+        public IHttpActionResult PostApprove()
         {
-            return Ok(body);
+            return Redirect("http://localhost:5403/thankyou.html");
         }
+
+        #region Encoding/Decoding
 
         private string ToUrlEncodedString(Dictionary<string, string> request)
         {
@@ -63,7 +75,6 @@
                 builder.Append("&");
                 builder.Append(key);
                 builder.Append("=");
-                //builder.Append(request[key]);
                 builder.Append(HttpUtility.UrlEncode(request[key]));
             }
 
@@ -80,45 +91,70 @@
             foreach (string valuePair in valuePairs)
             {
                 var values = valuePair.Split('=');
-                result.Add(values[0], values[1]);
+                result.Add(values[0], HttpUtility.UrlDecode(values[1]));
             }
 
             return result;
         }
 
-        private string GetMd5Hash(Dictionary<string, string> request, string key)
+        #endregion Encoding/Decoding
+
+        #region MD5 Hashing
+
+        // Adapted from
+        // https://msdn.microsoft.com/en-us/library/system.security.cryptography.md5(v=vs.110).aspx
+
+        private string GetMd5Hash(Dictionary<string, string> data, string encryptionKey)
         {
             using (MD5 md5Hash = MD5.Create())
             {
                 var input = new StringBuilder();
-                foreach (string value in request.Values)
+                foreach (string value in data.Values)
                 {
                     input.Append(value);
                 }
 
-                input.Append(key);
+                input.Append(encryptionKey);
 
-                var test = input.ToString();
+                byte[] hash = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input.ToString()));
 
-                // Convert the input string to a byte array and compute the hash.
-                byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input.ToString()));
-
-                // Create a new Stringbuilder to collect the bytes
-                // and create a string.
                 StringBuilder sBuilder = new StringBuilder();
 
-                // Loop through each byte of the hashed data 
-                // and format each one as a hexadecimal string.
-                for (int i = 0; i < data.Length; i++)
+                for (int i = 0; i < hash.Length; i++)
                 {
-                    sBuilder.Append(data[i].ToString("x2"));
+                    sBuilder.Append(hash[i].ToString("x2"));
                 }
-
-                // Return the hexadecimal string.
                 return sBuilder.ToString();
             }
         }
 
+        private bool VerifyMd5Hash(Dictionary<string, string> data, string encryptionKey, string hash)
+        {
+            var hashDict = new Dictionary<string, string>();
 
+            foreach (string key in data.Keys)
+            {
+                if (key != "CHECKSUM")
+                {
+                    hashDict.Add(key, data[key]);
+                }
+            }
+
+            string hashOfInput = GetMd5Hash(hashDict, encryptionKey);
+
+            StringComparer comparer = StringComparer.OrdinalIgnoreCase;
+
+            if (0 == comparer.Compare(hashOfInput, hash))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+        }
+
+        #endregion MD5 Hashing
     }
 }
